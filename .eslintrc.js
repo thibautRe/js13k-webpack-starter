599dfa6362f3c63ed45c20c918fa6f42809296ec
Thibaut REMY <thibaut@thibaut.re>
2018-08-01 19:55:42 +0200

    :new: :package: Add ESLint and Prettier toolchain

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'prettier',
+  ],
+  env: {
+    browser: true,
+  },
+  plugins: [
+    'prettier',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": ['error', {
+      semi: false,
+      singleQuote: true,
+      trailingComma: 'all',
+    }],
+  },
+}
