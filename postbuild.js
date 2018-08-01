408ae6db212dec96c15c68e996cb3d494102bf07
Thibaut REMY <thibaut@thibaut.re>
2018-08-01 19:56:33 +0200

    :sparkles: Add size logging in postbuild
    Use chalk for colors
    Y

--- postbuild.js
+++ postbuild.js
@@ -1,34 +1,53 @@
+/* eslint-env node */
+/* eslint-disable no-console */
+
 const fs = require('fs')
 const archiver = require('archiver')
-
-fs.unlinkSync('./dist/script.js')
-fs.unlinkSync('./dist/style.css')
+const chalk = require('chalk')
 
 let output = fs.createWriteStream('./dist/build.zip')
 let archive = archiver('zip', {
-  zlib: { level: 9 } // set compression to best
+  zlib: { level: 9 }, // set compression to best
 })
 
+const MAX = 13 * 1024 // 13kb
+
 output.on('close', function() {
-  console.log(archive.pointer() + '/13312 total bytes');
-});
+  const bytes = archive.pointer()
+  let color = 'green'
+
+  if (bytes > MAX) {
+    color = 'red'
+    console.error(chalk.red('⛔  Size overflow ⛔'))
+  } else if (bytes > MAX * 0.9) {
+    color = 'yellow'
+    console.warn(chalk.yellow('⚠️  Approaching maximum size'))
+  }
+
+  const percent = `${Math.round((bytes * 100) / MAX)}%`
+  const allBytes = `${bytes.toString()}/${MAX}`
+  console.log(chalk[color](`${allBytes} total bytes (${chalk.bold(percent)})`))
+
+  if (bytes > MAX) {
+    process.exit(1)
+  }
+})
 
 archive.on('warning', function(err) {
   if (err.code === 'ENOENT') {
     console.warn(err)
   } else {
-    throw err;
+    throw err
   }
-});
+})
 
 archive.on('error', function(err) {
-  throw err;
-});
+  throw err
+})
 
-archive.pipe(output);
-archive.append(
-  fs.createReadStream('./dist/index.html'), {
-    name: 'index.html'
-  })
+archive.pipe(output)
+archive.append(fs.createReadStream('./dist/index.html'), {
+  name: 'i.html',
+})
 
 archive.finalize()
